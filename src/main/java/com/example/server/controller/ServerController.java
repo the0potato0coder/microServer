02967f14df700c13e38114f9ba395da195fb5c49
the0potato0coder <jugantarroy27@gmail.com>
2025-11-22 10:14:45 +0530

    Initial Commit

---
+++ src/main/java/com/example/server/controller/ServerController.java
@@ -0,0 +1,7 @@
+package com.example.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+}
---
+++ src/main/java/com/example/server/model/Server.java
@@ -0,0 +1,9 @@
+package com.example.server.model;
+
+public class Server {
+
+    private int transactionId;
+    private String name;
+    private String email;
+    private int phoneNo;
+}
